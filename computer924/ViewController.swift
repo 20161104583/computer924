708f44f563f0336e160c07f343b682dfede0a6e5
20161104583 <dorgen@CIEC-iMacs-iMac.local>
2018-10-10 10:14:12 +0800

    Computer924
    Computer

--- computer924/ViewController.swift
+++ computer924/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var comx: UITextField!
+    
+    @IBOutlet var comy: UITextField!
+    
+    @IBOutlet var comz: UITextField!
+    
+    
+    @IBAction func test(_ sender: Any) {
+        comz.text = "\(Int(comx.text!)! + Int(comy.text!)!)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
